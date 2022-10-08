fdc7ab5701ab9303b0b5458ad7bd73f519d42a70
revsic <revsic99@gmail.com>
2022-10-09 00:05:02 +0900

    aug: Add equalizer

--- utils/augment.py
+++ utils/augment.py
@@ -1,6 +1,9 @@
+from typing import Optional
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchaudio.functional as AF
 
 from .lpc import LinearPredictiveCoding
 
@@ -26,42 +29,78 @@ class Augment(nn.Module):
 
     def forward(self,
                 wavs: torch.Tensor,
-                pitch_shift: torch.Tensor,
-                formant_shift: torch.Tensor,
+                pitch_shift: Optional[torch.Tensor] = None,
+                formant_shift: Optional[torch.Tensor] = None,
+                quality_power: Optional[torch.Tensor] = None,
+                gain: Optional[torch.Tensor] = None,
                 mode: str = 'linear') -> torch.Tensor:
         """Augment the audio signal, random pitch, formant shift and PEQ.
         Args:
             wavs: [torch.float32; [B, T]], audio signal.
             pitch_shift: [torch.float32; [B]], pitch shifts.
             formant_shift: [torch.float32; [B]], formant shifts.
+            quality_power: [torch.float32; [B, num_peak + 2]],
+                exponents of quality factor, for PEQ.
+            gain: [torch.float32; [B, num_peak]], gain in decibel.
             mode: interpolation mode, `linear` or `nearest`.
         Returns:
             [torch.float32; [B, T]], augmented.
         """
-        # [B, F, T / S], complex64
-        fft = torch.stft(
-            wavs,
-            self.config.data.fft,
-            self.config.data.hop,
-            self.config.data.win,
-            self.window,
-            return_complex=True)
-        # [B, T / S, num_code]
-        code = self.coder.from_stft(fft)
-        # [B, T / S, F]
-        filter_ = self.coder.envelope(code)
-        source = fft.transpose(1, 2) / (filter_ + 1e-7)
-            # filter_[source.isnan()])
-        # [B, T / S, F]
-        aug_filter = self.interp(filter_, formant_shift, mode=mode)
-        aug_source = self.interp(source, pitch_shift, mode=mode)
-        # [B, T]
-        return torch.istft(
-            (aug_source * aug_filter).transpose(1, 2),
-            self.config.data.fft,
-            self.config.data.hop,
-            self.config.data.win,
-            self.window)
+        # random formant, pitch shifter
+        if formant_shift is not None or pitch_shift is not None:
+            # [B, F, T / S], complex64
+            fft = torch.stft(
+                wavs,
+                self.config.data.fft,
+                self.config.data.hop,
+                self.config.data.win,
+                self.window,
+                return_complex=True)
+            # [B, T / S, num_code]
+            code = self.coder.from_stft(fft)
+            # [B, T / S, F]
+            filter_ = self.coder.envelope(code)
+            source = fft.transpose(1, 2) / (filter_ + 1e-7)
+            # [B, T / S, F]
+            if formant_shift is not None:
+                filter_ = self.interp(filter_, formant_shift, mode=mode)
+            if pitch_shift is not None:
+                source = self.interp(source, pitch_shift, mode=mode)
+            # [B, T]
+            wavs = torch.istft(
+                (source * filter_).transpose(1, 2),
+                self.config.data.fft,
+                self.config.data.hop,
+                self.config.data.win,
+                self.window)
+        # PEQ
+        if quality_power is not None:
+            # alias
+            lowcut, highcut, num_peak, sr = \
+                self.config.train.cutoff_lowpass, \
+                self.config.train.cutoff_highpass, \
+                self.config.train.num_peak, \
+                self.config.data.sr
+            # [B, num_peak + 2]
+            qualities = self.config.train.q_min * (
+                self.config.train.q_max / self.config.train.q_min) ** quality_power
+            if gain is None:
+                # [B, num_peak]
+                gain = torch.ones_like(qualities[:, :-2])
+            # num_peak x ([B], [B])
+            for i, (g, q) in enumerate(zip(gain.T, qualities.T)):
+                center = lowcut * (highcut / lowcut) ** (i / (num_peak - 1))
+                # [B, T]
+                wavs = [
+                    AF.equalizer_biquad(wav, sr, center, g.item(), q.item())
+                    for wav in wavs]
+            # [B, T], lowpass
+            wavs = torch.stack([
+                AF.lowpass_biquad(
+                    AF.highpass_biquad(wav, sr, highcut, hq.item()),
+                    sr, lowcut, lq.item())
+                for wav, (lq, hq) in zip(wavs, qualities[:, -2:])], dim=0)
+        return wavs
 
     @staticmethod
     def complex_interp(inputs: torch.Tensor, *args, **kwargs) -> torch.Tensor:
