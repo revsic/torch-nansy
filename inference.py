ab10b992724f4bdcf4ec37f051e5482d721e0fb8
revsic <revsic99@gmail.com>
2022-12-19 13:05:00 +0000

    inference: Fix inference

--- inference.py
+++ inference.py
@@ -1,8 +1,6 @@
 import argparse
-import os
 
 import librosa
-import numpy as np
 import torch
 
 from nansy import Nansy
@@ -13,9 +11,9 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--ckpt', default=None)
 parser.add_argument('--hifi-ckpt', default=None)
 parser.add_argument('--hifi-config', default=None)
-parser.add_argument('--wav1', default=None)
-parser.add_argument('--wav2', default=None)
-parser.add_argument('--out-dir', default='./outputs')
+parser.add_argument('--context', default=None)
+parser.add_argument('--identity', default=None)
+parser.add_argument('--out-path', default='./outputs/out.wav')
 args = parser.parse_args()
 
 # load checkpoint
@@ -30,47 +28,32 @@ hifigan = HiFiGANWrapper(args.hifi_config, args.hifi_ckpt, device)
 
 # load wav
 SR = nansy.config.sr
-wav1, _ = librosa.load(args.wav1, sr=SR)
-wav2, _ = librosa.load(args.wav2, sr=SR)
-wavs = [wav1, wav2]
-# pack
-wavlen = np.array([len(w) for w in wavs])
-wav = np.stack([np.pad(w, [0, wavlen.max() - len(w)]) for w in wavs], axis=0)
-# convert
-wavlen = torch.tensor(wavlen, device=device)
-wav = torch.tensor(wav, device=device)
+context, _ = librosa.load(args.context, sr=SR)
+identity, _ = librosa.load(args.identity, sr=SR)
 
 with torch.no_grad():
-    # [B, mel, T] reconstruction
-    out, aux = nansy.forward(wav, wavlen)
-    print(f'[*] reconstruct {out.shape}')
+    context = torch.tensor(context[None], device=device)
+    # TODO: pitch shift to match media pitch
+    feat = nansy.analyze(context)
+
+    # [1, T]
+    identity = torch.tensor(identity[None], device=device)
+    # [1, ver_out_channels]
+    _, spk = nansy.analyze_wav2vec2(identity)
+
+    # [1, mel_filters, T / mel_strides]
+    mel, _, _ = nansy.synthesize(
+        feat['linguistic'],
+        spk,
+        feat['energy'],
+        nansy.sample_yingram(feat['yingram']))
+
     # vocoding
-    out = hifigan.forward(out)
-    out = out / out.max(dim=-1).values.clamp_min(1e-7)[:, None]
+    out = hifigan.forward(mel).clamp(-1, 1).squeeze(dim=0)
+    # []
+    maxval = out.abs().max(dim=-1).values
+    # [T]
+    out = out / maxval.clamp_min(1e-7)
     print(f'[*] done, {out.shape}')
 
-    HOP = 256
-    for i, (w, l) in enumerate(zip(out, wavlen)):
-        librosa.output.write_wav(
-            os.path.join(args.out_dir, f'rctor{i}.wav'),
-            w.cpu().numpy()[:l.item() * HOP],
-            sr=SR)
-
-    spk = aux['spk']
-    spk = torch.flip(spk, dims=(0,))
-    # vc
-    out, _, _ = nansy.synthesize(
-        aux['linguistic'], spk, aux['energy'], aux['yingram'])
-    print(f'[*] vc {out.shape}')
-
-    out = hifigan.forward(out)
-    out = out / out.max(dim=-1).values.clamp_min(1e-7)[:, None]
-    print(f'[*] done, {out.shape}')
-
-    HOP = 256
-    for i, (w, l) in enumerate(zip(out, wavlen)):
-        librosa.output.write_wav(
-            os.path.join(args.out_dir, f'vc{i}.wav'),
-            w.cpu().numpy()[:l.item() * HOP],
-            sr=SR)
-
+    librosa.output.write_wav(args.out_path, out.cpu().numpy(), sr=SR)
