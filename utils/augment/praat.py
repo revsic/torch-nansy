48eeed30df7fae4827d52a4d0b912765be066661
revsic <revsic99@gmail.com>
2023-01-17 00:59:30 +0000

    praat: Add praat augment

--- utils/augment/praat.py
+++ utils/augment/praat.py
@@ -1,11 +1,8 @@
-import os
-import multiprocessing as mp
-from typing import Callable, Optional, Tuple, Union
+from typing import Union
 
 import numpy as np
 import parselmouth
 
-import speechset
 from config import Config
 
 
@@ -47,7 +44,14 @@ class PraatAugment:
         if nonzero.sum() == 0:
             return snd.values[0]
         # if voiced
-        median = np.median(ndpit[nonzero]).item()
+        median, minp = np.median(ndpit[nonzero]).item(), ndpit[nonzero].min().item()
+        # scale
+        updated = median * pitch_shift
+        scaled = updated + (minp * pitch_shift - updated) * pitch_range
+        # for preventing infinite loop of `Change gender`
+        # ref:https://github.com/praat/praat/issues/1926
+        if scaled < 0.:
+            pitch_range = 1.
         out, = parselmouth.praat.call(
             (snd, pitch), 'Change gender',
             formant_shift,
@@ -55,109 +59,3 @@ class PraatAugment:
             pitch_range,
             duration_factor).values
         return out
-
-    def augment_wrap(self, wrap: Tuple[np.ndarray, float, float]) -> np.ndarray:
-        audio, fs, ps = wrap
-        return self.augment(audio, fs, ps)
-
-    def cache(self,
-              out_dir: str,
-              reader: speechset.datasets.DataReader,
-              num_ps: int = 5,
-              num_fs: int = 5,
-              verbose: Optional[Callable] = None,
-              pool: Optional[Union[int, mp.Pool]] = None,
-              chunksize: int = 1):
-        """Cache the contextual features.
-        Args:
-            out_dir: output directory.
-            reader: data reader.
-            num_ps: the number of the candidates for pitch shifting.
-            num_ps: the number of the candidates for formant shifting.
-            verbose: whether write the progressbar or not.
-        """
-        dataset, preproc = reader.dataset(), reader.preproc()
-        if verbose is not None:
-            dataset = verbose(dataset)
-
-        ps = np.linspace(1., self.config.train.pitch_shift, num_ps)
-        fs = np.linspace(1., self.config.train.formant_shift, num_fs)
-
-        close_pool = False
-        if pool is not None and isinstance(pool, int):
-            close_pool = True
-            pool = mp.Pool(pool)
-
-        os.makedirs(out_dir, exist_ok=True)
-        for path in dataset:
-            # [T]
-            outputs = preproc(path)
-            assert len(outputs) in [2, 3]
-            if len(outputs) == 2:
-                _, audio = outputs
-            else:
-                _, _, audio = outputs
-            # cache the augment
-            augmented = []
-            if pool is None:
-                # wrap
-                snd = parselmouth.Sound(audio, sampling_frequency=self.config.model.sr)
-                for p in ps:
-                    for f in fs:
-                        augmented.extend([
-                            self.augment(snd, f, p),
-                            self.augment(snd, 1 / f, p),
-                            self.augment(snd, f, 1 / p),
-                            self.augment(snd, 1 / f, 1 / p)])
-            else:
-                for out in pool.imap_unordered(
-                        self.augment_wrap,
-                        [
-                            (audio, f_, p_)
-                            for p in ps
-                            for f in fs
-                            for f_, p_ in [
-                                (f, p),
-                                (1 / f, p),
-                                (f, 1 / p),
-                                (1 / f, 1 / p)]],
-                        chunksize=chunksize):
-                    augmented.append(out)
-
-            # write
-            name, _ = os.path.splitext(os.path.basename(path))
-            np.save(os.path.join(out_dir, f'{name}.npy'), np.stack(augmented, axis=0))
-
-        if close_pool:
-            pool.close()
-
-
-if __name__ == '__main__':
-    def main():
-        import argparse
-        from tqdm import tqdm
-
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--data-dir', required=True)
-        parser.add_argument('--out-dir', required=True)
-        parser.add_argument('--num-ps', default=3, type=int)
-        parser.add_argument('--num-fs', default=3, type=int)
-        parser.add_argument('--pool', default=None, type=int)
-        parser.add_argument('--chunksize', default=1, type=int)
-        args = parser.parse_args()
-
-        # hard code the reader
-        reader = speechset.utils.DumpReader(args.data_dir)
-
-        config = Config()
-        aug = PraatAugment(config)
-        aug.cache(
-            args.out_dir,
-            reader,
-            args.num_ps,
-            args.num_fs,
-            tqdm,
-            args.pool,
-            args.chunksize)
-
-    main()
